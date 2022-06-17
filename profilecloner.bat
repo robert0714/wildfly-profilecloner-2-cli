cda51175f2d9664be4dc7adeb17e3d806b41b0be
Robert <robert0714@gmail.com>
2022-06-17 12:18:22 +0800

    Update profilecloner.bat
    adjust artifact path

--- profilecloner.bat
+++ profilecloner.bat
@@ -1,4 +1,4 @@
 
 rem  set "JBOSS_HOME=c:\path\to\jboss"
 
-java -cp %JBOSS_HOME%\bin\client\jboss-cli-client.jar;.\profilecloner.jar org.jboss.tfonteyne.profilecloner.Main %*
+java -cp %JBOSS_HOME%\bin\client\jboss-cli-client.jar;.\target\profilecloner.jar org.jboss.tfonteyne.profilecloner.Main %*
